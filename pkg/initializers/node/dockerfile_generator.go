2dca8d79048f3570b4d37ab03fa5830c8e60db92
Scott Andrews <sandrews@pivotal.io>
2018-02-14 13:01:50 -0500

    Add support for node packages
    A node package is distinguished from a node module by a package.json
    file. A package can contain one or more modules. The package.json file
    provides a wealth of metadata including the dependencies to be
    installed, the main entry module into the package, custom scripts, and
    more.
    
    When a package.json file is present these key changes should occur:
    1. all files in the directory are copied into the container
    2. production dependencies are installed via npm in the docker daemon
    3. the entry point should be defined by the package, and the invoker
       should require the directory instead of a specific module
    
    The package.json file can also provide a conveient hook to apply source
    transformations via a postinstall script. For example, compiling
    TypeScript, or ES.next via Babel.
    
    Closes projectriff/node-function-invoker#19

--- pkg/initializers/node/dockerfile_generator.go
+++ pkg/initializers/node/dockerfile_generator.go
@@ -17,23 +17,49 @@
 package node
 
 import (
-	"github.com/projectriff/riff-cli/pkg/options"
 	"path/filepath"
+
 	"github.com/projectriff/riff-cli/pkg/initializers/core"
+	"github.com/projectriff/riff-cli/pkg/options"
+	"github.com/projectriff/riff-cli/pkg/osutils"
 )
+
+type NodeDockerFileTokens struct {
+	core.DockerFileTokens
+	PackageJSONExists bool
+}
 
 var nodeFunctionDockerfileTemplate = `
 FROM projectriff/node-function-invoker:{{.RiffVersion}}
+{{ if .PackageJSONExists -}}
+ENV FUNCTION_URI /functions/
+COPY . ${FUNCTION_URI}
+RUN (cd ${FUNCTION_URI} && npm install --production)
+{{- else -}}
 ENV FUNCTION_URI /functions/{{.Artifact}}
 ADD {{.ArtifactBase}} ${FUNCTION_URI}
+{{- end }}
 `
 
+// TODO create .dockerignore file
+var nodeFunctionDockerIgnoreTemplate = `
+{{ if .PackageJSONExists -}}
+node_modules
+{{- end }}
+`
 
 func generateNodeFunctionDockerFile(opts options.InitOptions) (string, error) {
-	dockerFileTokens := core.DockerFileTokens{
-		Artifact:     opts.Artifact,
-		ArtifactBase: filepath.Base(opts.Artifact),
-		RiffVersion:  opts.RiffVersion,
-	}
+	dockerFileTokens := NodeDockerFileTokens{}
+	dockerFileTokens.Artifact = opts.Artifact
+	dockerFileTokens.ArtifactBase = filepath.Base(opts.Artifact)
+	dockerFileTokens.RiffVersion = opts.RiffVersion
+	dockerFileTokens.PackageJSONExists = packageJSONExists(opts.FilePath)
 	return core.GenerateFunctionDockerFileContents(nodeFunctionDockerfileTemplate, "docker-node", dockerFileTokens)
+}
+
+func packageJSONExists(filePath string) bool {
+	if !osutils.IsDirectory(filePath) {
+		filePath = filepath.Dir(filePath)
+	}
+	return osutils.FileExists(filepath.Join(filePath, "package.json"))
 }
